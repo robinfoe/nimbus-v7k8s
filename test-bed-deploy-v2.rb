f161d35b74648c710be5eb1ab6e9fd0bd212e67d
robinfoe <robin@rfoe-a01.vmware.com>
2020-12-02 11:49:45 +0800

    add postboot

--- test-bed-deploy-v2.rb
+++ test-bed-deploy-v2.rb
@@ -4,17 +4,26 @@ $testbed = Proc.new do
   {
     "name" => "testbed-test",
     "version" => 3,
+
+    'worker' => [
+        {
+            'name' => "worker.0",
+            'enableStaticIpService' => true, # turn on static ip server
+        },
+    ],
+
     "esx" => (0..2).map do | idx |
       {
         "name" => "esx.#{idx}",
         "vc" => "vc.0",
         "customBuild" => "ob-17168206",
-        "dc" => "vcqaDC",
-        "clusterName" => "cluster0",
+        "dc" => "dc-apj",
+        "clusterName" => "cluster-k8s",
         "style" => "fullInstall",
-        "cpus" => 8, # 32 vCPUs
-        "memory" => 24000, # 98000 98GB memory
+        "cpus" => 16, # 32 vCPUs
+        "memory" => 32000, # 98000 98GB memory
         "fullClone" => true,
+        "nic" => 2,
         "desiredPassword" => "ca$hc0w",
         "disks" => [ 200 * oneGB ], # [ 2 * 1000 * oneGB ] -->  2 TB Disk
         "ssd" => [ 50 * oneGB, 600 * oneGB ] # [ 2 * 1000 * oneGB ] -->  2 TB Disk
@@ -26,12 +35,12 @@ $testbed = Proc.new do
         "name" => "vc.0",
         "type" => "vcva",
         "customBuild" => "ob-17004997",
-        "dcName" => ["vcqaDC"],
+        "dcName" => ["dc-apj"],
         "enableDrs" => true,
         "clusters" => [
           {
-            "name" => "cluster0",
-            "dc" => "vcqaDC"
+            "name" => "cluster-k8s",
+            "dc" => "dc-apj"
           }
         ],
         "disks" => [ 600 * oneGB ]
@@ -41,6 +50,8 @@ $testbed = Proc.new do
     "beforePostBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
     end,
     "postBoot" => Proc.new do |runId, testbedSpec, vmList, catApi, logDir|
+      workerVM = vmList['worker'][0]
+      Log.info "static ip service endpoint is #{workerVM.info['nsips']}"
     end
   }
 end
